4bf478730a274993359756b63fca0e4f633bc37e
Arijit <arijitbasubd@gmail.com>
2025-01-18 04:43:46 +0600

    about building and coupon page redesigned

--- src/Shared/AboutBuilding.jsx
+++ src/Shared/AboutBuilding.jsx
@@ -5,22 +5,25 @@ const AboutBuilding = () => {
   return (
     <div>
       <Title heading={"about the building"}></Title>
-      <div className=" mt-10 p-2 bg-neutral-white">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+      <div className=" mt-10 p-2 flex flex-col md:flex-row bg-neutral-white">
+        <div className="md:w-3/5">
+          <img src="/bannerImg.jpeg" className="h-52 mb-10 md:mb-0 md:h-full w-full object-cover pr-2" alt="" />
+        </div>
+        <div className="space-y-10 md:w-2/5">
           {/* card */}
-          <div className="flex flex-col  text-center bg-neutral py-2">
+          <div className="flex flex-col text-center bg-neutral py-2">
             <h1 className="text-primary uppercase font-semibold border-b-4 border-accent text-2xl">
               Why choose us ?
             </h1>
 
             <p className="text-primary-light text-left px-2 mt-4 ">
-              <p className="badge bg-accent-orange text-neutral-white font-semibold p-4">
+              <p className="my-1 text-lg uppercase rounded-sm border-4 border-accent hover:border-accent-orange hover:bg-accent transition-all duration-300 ease-in-out bg-accent-orange text-neutral-white font-semibold p-4">
                 🌿 Eco-Friendly Design
               </p>
-              <p className="badge bg-accent-orange text-neutral-white font-semibold p-4">
+              <p className="my-1 text-lg uppercase rounded-sm border-4 border-accent hover:border-accent-orange hover:bg-accent transition-all duration-300 ease-in-out bg-accent-orange text-neutral-white font-semibold p-4">
                 🏢 Modern Architecture
               </p>
-              <p className="badge bg-accent-orange text-neutral-white font-semibold p-4">
+              <p className="my-1 text-lg uppercase rounded-sm border-4 border-accent hover:border-accent-orange hover:bg-accent transition-all duration-300 ease-in-out bg-accent-orange text-neutral-white font-semibold p-4">
                 🔒 24/7 Security
               </p>
             </p>
@@ -32,31 +35,31 @@ const AboutBuilding = () => {
             </h1>
 
             <p className="text-primary-light text-left px-2 mt-4">
-              <p className="badge bg-accent-orange text-neutral-white font-semibold p-4">
+              <p className="my-1 text-lg uppercase rounded-sm border-4 border-accent hover:border-accent-orange hover:bg-accent transition-all duration-300 ease-in-out bg-accent-orange text-neutral-white font-semibold p-4">
                 🏊 Swimming Pool
               </p>
-              <p className="badge bg-accent-orange text-neutral-white font-semibold p-4">
+              <p className="my-1 text-lg uppercase rounded-sm border-4 border-accent hover:border-accent-orange hover:bg-accent transition-all duration-300 ease-in-out bg-accent-orange text-neutral-white font-semibold p-4">
                 🏋️ Fitness Center
               </p>
-              <p className="badge bg-accent-orange text-neutral-white font-semibold p-4">
+              <p className="my-1 text-lg uppercase rounded-sm border-4 border-accent hover:border-accent-orange hover:bg-accent transition-all duration-300 ease-in-out bg-accent-orange text-neutral-white font-semibold p-4">
                 🚗 Parking
               </p>
             </p>
           </div>
           {/* card */}
-          <div className="flex flex-col  text-center bg-neutral py-2 md:col-span-2">
+          <div className="flex flex-col  text-center bg-neutral py-2">
             <h1 className="text-primary uppercase font-semibold border-b-4 border-accent text-2xl">
               Our Vibrant Location
             </h1>
 
             <p className="text-primary-light text-left px-2 mt-4">
-              <p className="badge bg-accent-orange text-neutral-white font-semibold p-4">
+              <p className="my-1 text-lg uppercase rounded-sm border-4 border-accent hover:border-accent-orange hover:bg-accent transition-all duration-300 ease-in-out bg-accent-orange text-neutral-white font-semibold p-4">
                 🛍️ Located near City Mall
               </p>
-              <p className="badge bg-accent-orange text-neutral-white font-semibold p-4">
+              <p className="my-1 text-lg uppercase rounded-sm border-4 border-accent hover:border-accent-orange hover:bg-accent transition-all duration-300 ease-in-out bg-accent-orange text-neutral-white font-semibold p-4">
                 🎓 Close to the best school of the city
               </p>
-              <p className="badge bg-accent-orange text-neutral-white font-semibold p-4">
+              <p className="my-1 text-lg uppercase rounded-sm border-4 border-accent hover:border-accent-orange hover:bg-accent transition-all duration-300 ease-in-out bg-accent-orange text-neutral-white font-semibold p-4">
                 🚇 5 minutes away from the nearest metro station
               </p>
             </p>
--- src/Shared/Coupons.jsx
+++ src/Shared/Coupons.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import Title from '../Components/Title';
+import { useQuery } from '@tanstack/react-query';
+import useAxiosPublic from '../Hooks/useAxiosPublic';
 
 const Coupons = () => {
+    const axiosPublic = useAxiosPublic();
+
+    const {data: coupons = []} = useQuery({
+        queryKey: ['coupons'],
+        queryFn: async()=>{
+           const res = await axiosPublic.get('/coupons')
+           return res.data
+
+        }
+    })
+    console.log(coupons);
     return (
-        <div className='h-[500px] bg-neutral-white'>
+        <div className='p-2'>
             <Title heading={"Coupons"}></Title>
-            <div className="">
-                <p className="text-5xl">coupons will be added after completing admin routes</p>
+            <div className="grid bg-neutral-white my-10 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 p-2">
+                {
+                    coupons.map(coupon => (
+                        <div className="bg-neutral/30 flex flex-col justify-center items-center border p-2 relative">
+                            <p className={`text-4xl absolute -top-5 -left-3 ${coupon.isAvailable? "text-success" : "text-error"}`}>{coupon.isAvailable ? "●": "●"}</p>
+                            <p className='font-josefin text-4xl font-extrabold text-accent-orange'>{coupon.discount}% <span className='font-semibold text-2xl'>off</span></p>
+                            <p className='text-accent font-semibold tex-lg'><span className='uppercase text-sm text-primary'>use:</span> {coupon.couponCode}</p>
+                            <p className='text-xs pt-2'>{coupon.description}</p>
+                        </div>
+                    ))
+                }
             </div>
         </div>
     );
